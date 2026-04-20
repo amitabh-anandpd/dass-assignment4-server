48e4106abca569effcbce7ae95eb0d3cd3d18a13
amitabh-anandpd <amitabh.anandpd@gmail.com>
2026-04-20 18:43:57 +0530

    Token views errors resolved, gitignore created

--- ServerTrialIMS/urls.py
+++ ServerTrialIMS/urls.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import health_check
+
+try:
+    from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+except ImportError:
+    TokenObtainPairView = None
+    TokenRefreshView = None
 
 urlpatterns = [
     path('health/', health_check, name='health_check'),
     path('admin/', admin.site.urls),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
     path('api/academics/', include('academics.urls')),
     path('api/exams/', include('exams.urls')),
     path('api/attendance/', include('attendance.urls')),
     path('api/reports/', include('reports.urls')),
 ]
+
+if TokenObtainPairView and TokenRefreshView:
+    urlpatterns += [
+        path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ]
